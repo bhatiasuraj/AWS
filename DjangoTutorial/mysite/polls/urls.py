77c8f13501fbb11fd2a6297cbe15024afc923658
Bhatia <subhati@8c8590bd020e.ant.amazon.com>
2018-04-04 09:19:50 -0500

    Django Tutorial

---
+++ DjangoTutorial/mysite/polls/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+
+    path('', views.index, name='Index'),
+
+    path('<question_id>', views.detail, name='detail'),
+
+    path('', views.index, name="index"),
+
+    url('(?P<question_id>[0-9]+)/detail$', views.detail, name="detail"),
+
+    url('(?P<question_id>[0-9]+)/results$', views.results, name="results"),
+
+    url('(?P<question_id>[0-9]+)/vote$', views.vote, name="vote"),
+
+path('', views.contact, name='Contact'),
+
+]
